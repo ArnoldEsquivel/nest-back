65f31e6931fc4f21bc53a854364838c30cb68bef
Arnold Esquivel <arnold_526@outlook.com>
2024-02-25 14:40:10 -0600

    lib(axios): Add axios library and nest axios module

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refac', 'test', 'ci']],
+        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refac', 'test', 'ci', 'lib']],
         'type-case': [2, 'always', 'lower-case'],
         'type-empty': [2, 'never'],
         'scope-case': [2, 'always', ['camel-case', 'kebab-case']],
