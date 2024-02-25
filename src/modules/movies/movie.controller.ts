e0d0fed983bd434d38ce8e3055048b3a217c383b
Arnold Esquivel <arnold_526@outlook.com>
2024-02-25 14:50:40 -0600

    feat(controller): Define main two routes for movies controller

--- src/modules/movies/movie.controller.ts
+++ src/modules/movies/movie.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { MovieService } from './movie.service'
 
+@ApiTags('movies')
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+	constructor(private readonly movieService: MovieService) {}
+
+	@Get('popular')
+	async getPopularMovies() {
+		return this.movieService.getPopularMovies()
+	}
+
+	@Get('search')
+	async searchMovies(@Query('title') title: string) {
+		return this.movieService.searchMoviesByTitle(title)
+	}
+}
